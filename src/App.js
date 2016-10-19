69c0a11ec07100ab09c92d8ad8d89402d2b20522
abraxasrex <jbreasor@gmail.com>
2016-10-19 01:54:23 -0700

    replacing boilerplate and es6 classes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const Nav = React.createClass ({
+  something () {
+    return {obj: 1};
+  },
+  render () {
+    return <div className='nav-full'>
+      <div className='nav-partial' style={{background: '#FFA07A'}}>Day</div>
+      <div className='nav-partial' style={{background: '#FF7F50'}}>Week</div>
+      <div className='nav-partial' style={{background: '#FF8C00'}}>Month</div>
+      <div className='nav-partial' style={{background: '#FFA500'}}>Year</div>
+    </div>
+  }
+});
+
+const Panel = React.createClass({
+  render() {
+    return (
+      <div className='panel-fluid'> I am a panel. current calendar is {this.props.currentCal || 'not set'} </div>
+    );
+  }
+});
+
+const Calendar = React.createClass({
+  render() {
+    return (
+      <div> I am a calendar.
+      <Panel currentCal={this.props.currentCal || 'not set'}/>
+      </div>
+    );
+  }
+});
+
+
+const App = React.createClass({
+  getInitialState() {
+    return { currentCal: 'day'};
+  },
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>Notifry</h1>
+        <h5>New Note</h5>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='app-container'>
+          <Nav />
+          <Calendar currentCal={this.state.currentCal} />
+        </div>
       </div>
     );
   }
-}
+});
 
 export default App;
