6bcc842be4e3b316f0e9004910436096422941ae
abraxasrex <jbreasor@gmail.com>
2016-10-19 02:43:00 -0700

    grokking events and automatic binding

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+const NavPill = React.createClass({
+  render () {
+    return <div className='nav-partial' onClick={() => this.props.setCal(this.props.pillName)}>{this.props.pillName}</div>
+  }
+});
+
 const Nav = React.createClass ({
-  something () {
-    return {obj: 1};
-  },
   render () {
     return <div className='nav-full'>
-      <div className='nav-partial' style={{background: '#FFA07A'}}>Day</div>
-      <div className='nav-partial' style={{background: '#FF7F50'}}>Week</div>
-      <div className='nav-partial' style={{background: '#FF8C00'}}>Month</div>
-      <div className='nav-partial' style={{background: '#FFA500'}}>Year</div>
+      <NavPill className='nav-partial' style={{background: '#FFA07A'}} pillName='Day' setCal={this.props.setCal}/>
+      <NavPill className='nav-partial' style={{background: '#FF7F50'}} pillName='Week' setCal={this.props.setCal}/>
+      <NavPill className='nav-partial' style={{background: '#FF8C00'}} pillName='Month' setCal={this.props.setCal}/>
+      <NavPill className='nav-partial' style={{background: '#FFA500'}} pillName='Year' setCal={this.props.setCal}/>
     </div>
   }
 });
@@ -35,11 +38,19 @@ const Calendar = React.createClass({
 });
 
 
+
 const App = React.createClass({
   getInitialState() {
     return { currentCal: 'day'};
   },
+  setCal (e) {
+    //e.preventDefault();
+    this.setState({
+      currentCal: e
+    });
+  },
   render() {
+    //this.setCal = this.setCal.bind(this);
     return (
       <div className="App">
         <div className="App-header">
@@ -48,7 +59,7 @@ const App = React.createClass({
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </div>
         <div className='app-container'>
-          <Nav />
+          <Nav setCal={this.setCal}/>
           <Calendar currentCal={this.state.currentCal} />
         </div>
       </div>
